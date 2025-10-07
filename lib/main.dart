0fd0c9d092ce61fdce0f69ce1ba45c69f058e92a
sherlyazkiah <sherlyazkiah12@gmail.com>
2025-10-07 23:15:08 +0700

    container

--- lib/main.dart
+++ lib/main.dart
@@ -10,152 +10,39 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter layout demo: Sherly',
-      home: buildHomePage('Strawberry Pavlova Recipe'),
-    );
-  }
-
-  Widget buildHomePage(String title) {
-    const titleText = Padding(
-      padding: EdgeInsets.all(20),
-      child: Text(
-        'Strawberry Pavlova',
-        style: TextStyle(
-          fontWeight: FontWeight.w800,
-          letterSpacing: 0.5,
-          fontSize: 30,
-        ),
-      ),
-    );
-
-    const subTitle = Text(
-      'Pavlova is a meringue-based dessert named after the Russian ballerina '
-      'Anna Pavlova. Pavlova features a crisp crust and soft, light inside, '
-      'topped with fruit and whipped cream.',
-      textAlign: TextAlign.center,
-      style: TextStyle(fontFamily: 'Georgia', fontSize: 25),
-    );
-
-    final stars = Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Icon(Icons.star, color: Colors.green[500]),
-        Icon(Icons.star, color: Colors.green[500]),
-        Icon(Icons.star, color: Colors.green[500]),
-        const Icon(Icons.star, color: Colors.black),
-        const Icon(Icons.star, color: Colors.black),
-      ],
-    );
-
-    final ratings = Container(
-      padding: const EdgeInsets.all(20),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          stars,
-          const Text(
-            '170 Reviews',
-            style: TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.w800,
-              fontFamily: 'Roboto',
-              letterSpacing: 0.5,
-              fontSize: 20,
-            ),
-          ),
-        ],
-      ),
-    );
-
-    const descTextStyle = TextStyle(
-      color: Colors.black,
-      fontWeight: FontWeight.w800,
-      fontFamily: 'Roboto',
-      letterSpacing: 0.5,
-      fontSize: 18,
-      height: 2,
-    );
-
-    // DefaultTextStyle.merge() allows you to create a default text
-    // style that is inherited by its child and all subsequent children.
-    final iconList = DefaultTextStyle.merge(
-      style: descTextStyle,
-      child: Container(
-        padding: const EdgeInsets.all(20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Column(
-              children: [
-                Icon(Icons.kitchen, color: Colors.green[500]),
-                const Text('PREP:'),
-                const Text('25 min'),
-              ],
-            ),
-            Column(
-              children: [
-                Icon(Icons.timer, color: Colors.green[500]),
-                const Text('COOK:'),
-                const Text('1 hr'),
-              ],
-            ),
-            Column(
-              children: [
-                Icon(Icons.restaurant, color: Colors.green[500]),
-                const Text('FEEDS:'),
-                const Text('4-6'),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-
-    final leftColumn = Container(
-      padding: const EdgeInsets.fromLTRB(20, 30, 20, 20),
-      child: Column(children: [titleText, subTitle, ratings, iconList]),
-    );
-
-    final mainImage = Image.asset('images/pavlova.jpg', fit: BoxFit.cover);
-
-    return Scaffold(
-      appBar: AppBar(title: Text(title)),
-      body: LayoutBuilder(
-        builder: (context, constraints) {
-          if (constraints.maxWidth > 600) {
-            // Layout lebar (desktop/tablet)
-            return Center(
-              child: Card(
-                margin: const EdgeInsets.all(20),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Expanded(flex: 2, child: leftColumn),
-                    Expanded(flex: 3, child: mainImage),
-                  ],
-                ),
-              ),
-            );
-          } else {
-            // Layout sempit (HP)
-            return SingleChildScrollView(
-              child: Card(
-                margin: const EdgeInsets.all(20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    mainImage,
-                    leftColumn,
-                  ],
-                ),
-              ),
-            );
-          }
-        },
+      title: 'Container: Sherly',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Container: Sherly')),
+        body: Center(child: _buildImageColumn()),
       ),
     );
   }
-}
+
+  Widget _buildImageColumn() {
+    return Container(
+      decoration: const BoxDecoration(color: Colors.black26),
+      child: Column(children: [_buildImageRow(1), _buildImageRow(3)]),
+    );
+  }
+  
+  Widget _buildDecoratedImage(int imageIndex) => Expanded(
+    child: Container(
+      decoration: BoxDecoration(
+        border: Border.all(width: 10, color: Colors.black38),
+        borderRadius: const BorderRadius.all(Radius.circular(8)),
+      ),
+      margin: const EdgeInsets.all(4),
+      child: Image.asset('images/pict$imageIndex.jpg'),
+    ),
+  );
+
+  Widget _buildImageRow(int imageIndex) => Row(
+    children: [
+      _buildDecoratedImage(imageIndex),
+      _buildDecoratedImage(imageIndex + 1),
+    ],
+  );
+}  
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
