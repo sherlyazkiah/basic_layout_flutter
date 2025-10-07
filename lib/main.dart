f7aaf1cde88c6c6bd89d9de0621272840ad91300
sherlyazkiah <sherlyazkiah12@gmail.com>
2025-10-07 23:39:47 +0700

    listview

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const String appTitle = 'GridView: Sherly';
+    const String appTitle = 'ListView: Sherly';
 
     return MaterialApp(
       title: appTitle,
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
           title: const Text(appTitle),
         ),
         body: Center(
-          child: _buildGrid(),
+          child: _buildList(),
         ),
       ),
     );
@@ -37,6 +37,39 @@ class MyApp extends StatelessWidget {
   // a list when objects have a predictable naming pattern.
   List<Widget> _buildGridTileList(int count) =>
       List.generate(count, (i) => Image.asset('images/pic$i.jpg'));
+
+  Widget _buildList() {
+    return ListView(
+      children: [
+        _tile('CineArts at the Empire', '85 W Portal Ave', Icons.theaters),
+        _tile('The Castro Theater', '429 Castro St', Icons.theaters),
+        _tile('Alamo Drafthouse Cinema', '2550 Mission St', Icons.theaters),
+        _tile('Roxie Theater', '3117 16th St', Icons.theaters),
+        _tile(
+          'United Artists Stonestown Twin',
+          '501 Buckingham Way',
+          Icons.theaters,
+        ),
+        _tile('AMC Metreon 16', '135 4th St #3000', Icons.theaters),
+        const Divider(),
+        _tile('K\'s Kitchen', '757 Monterey Blvd', Icons.restaurant),
+        _tile('Emmy\'s Restaurant', '1923 Ocean Ave', Icons.restaurant),
+        _tile('Chaiya Thai Restaurant', '272 Claremont Blvd', Icons.restaurant),
+        _tile('La Ciccia', '291 30th St', Icons.restaurant),
+      ],
+    );
+  }
+
+  ListTile _tile(String title, String subtitle, IconData icon) {
+    return ListTile(
+      title: Text(
+        title,
+        style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 20),
+      ),
+      subtitle: Text(subtitle),
+      leading: Icon(icon, color: Colors.blue[500]),
+    );
+  }
 }
 
 class MyHomePage extends StatefulWidget {
