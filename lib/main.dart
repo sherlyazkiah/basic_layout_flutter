18d4070d6ac2949737b7a8d18b87226965a30b8b
sherlyazkiah <sherlyazkiah12@gmail.com>
2025-10-07 23:44:58 +0700

    stack

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const String appTitle = 'ListView: Sherly';
+    const String appTitle = 'Stack: Sherly';
 
     return MaterialApp(
       title: appTitle,
@@ -18,56 +18,32 @@ class MyApp extends StatelessWidget {
           title: const Text(appTitle),
         ),
         body: Center(
-          child: _buildList(),
+          child: _buildStack(),
         ),
       ),
     );
   }
 
-  Widget _buildGrid() => GridView.extent(
-    maxCrossAxisExtent: 150,
-    padding: const EdgeInsets.all(4),
-    mainAxisSpacing: 4,
-    crossAxisSpacing: 4,
-    children: _buildGridTileList(15),
-  );
-
-  // The images are saved with names pic0.jpg, pic1.jpg...pic29.jpg.
-  // The List.generate() constructor allows an easy way to create
-  // a list when objects have a predictable naming pattern.
-  List<Widget> _buildGridTileList(int count) =>
-      List.generate(count, (i) => Image.asset('images/pic$i.jpg'));
-
-  Widget _buildList() {
-    return ListView(
+  Widget _buildStack() {
+    return Stack(
+      alignment: const Alignment(0.6, 0.6),
       children: [
-        _tile('CineArts at the Empire', '85 W Portal Ave', Icons.theaters),
-        _tile('The Castro Theater', '429 Castro St', Icons.theaters),
-        _tile('Alamo Drafthouse Cinema', '2550 Mission St', Icons.theaters),
-        _tile('Roxie Theater', '3117 16th St', Icons.theaters),
-        _tile(
-          'United Artists Stonestown Twin',
-          '501 Buckingham Way',
-          Icons.theaters,
+        const CircleAvatar(
+          backgroundImage: AssetImage('images/pic0.jpg'),
+          radius: 100,
         ),
-        _tile('AMC Metreon 16', '135 4th St #3000', Icons.theaters),
-        const Divider(),
-        _tile('K\'s Kitchen', '757 Monterey Blvd', Icons.restaurant),
-        _tile('Emmy\'s Restaurant', '1923 Ocean Ave', Icons.restaurant),
-        _tile('Chaiya Thai Restaurant', '272 Claremont Blvd', Icons.restaurant),
-        _tile('La Ciccia', '291 30th St', Icons.restaurant),
+        Container(
+          decoration: const BoxDecoration(color: Colors.black45),
+          child: const Text(
+            'Sherly',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ),
       ],
-    );
-  }
-
-  ListTile _tile(String title, String subtitle, IconData icon) {
-    return ListTile(
-      title: Text(
-        title,
-        style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 20),
-      ),
-      subtitle: Text(subtitle),
-      leading: Icon(icon, color: Colors.blue[500]),
     );
   }
 }
